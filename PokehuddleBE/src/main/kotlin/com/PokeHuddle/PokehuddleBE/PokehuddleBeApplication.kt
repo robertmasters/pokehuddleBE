94acb19c3b4f900754c8f111515236e41050e41c
Robert-Rodriguez <robertmasters@hotmail.com>
2021-07-29 15:44:23 -0400

    starter kotlin backend

---
+++ PokehuddleBE/src/main/kotlin/com/PokeHuddle/PokehuddleBE/PokehuddleBeApplication.kt
@@ -0,0 +1,11 @@
+package com.PokeHuddle.PokehuddleBE
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokehuddleBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokehuddleBeApplication>(*args)
+} 
